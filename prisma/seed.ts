89f1f25a7dcb27420f4638ff8fba17b03baf3732
Alihdnz <alissondinizleo@gmail.com>
2026-02-15 17:08:36 -0300

    fix: fix error on declarationt

---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient, Role } from "@prisma/client";
+import { PrismaPG } from "@prisma/adapter-pg";
+
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient({
+    adapter: new PrismaPg({  connectionString: process.env.DATABASE_URL!  }),
+});
+
+async function main(){
+    const email = "alissondinizleo@gmail.com";
+    const passwordPlain = "@leonardO1997";
+    const password = await bcrypt.hash(passwordPlain, 10);
+
+    await prisma.user.upsert({
+        where: {email}, 
+        update: {password, role: Role.ADMIN, name: "Alisson"},
+        create: {email, name: "Alisson", password, role: Role.ADMIN},
+    });
+
+    console.log("seed ok:")
+    console.log("ADMIN:", `${email} / ${passwordPlain}`);
+
+}
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+}).finally(async () => prisma.$disconnect());
